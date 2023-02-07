eab6d212990fafe3d580d3a040c6976a60da832f
ire <100293388+irenetrrs@users.noreply.github.com>
2023-02-07 14:44:28 +0100

    light mode

--- app/src/main/java/com/example/futura_app/MainActivity.java
+++ app/src/main/java/com/example/futura_app/MainActivity.java
@@ -1,15 +1,18 @@
 package com.example.futura_app;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
+
 import android.os.Bundle;
 import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
         Log.d("--->> MainActivity", "Primer mensaje para depurar");
     }
 }
